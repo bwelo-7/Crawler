558fb5f4c446dcfaa1d3eed1aeef8599efd247d9
ryan <github@ryanrae.com>
2025-06-18 18:09:13 +0100

    if the joystick isnt moved enough then the input isnt registered

--- Controller.py
+++ Controller.py
@@ -10,3 +10,11 @@ joystick.init()
 
 left_x = joystick.get_axis(0)
 left_y = joystick.get_axis(1)
+
+deadzone = 0.4
+
+if abs(left_x) < deadzone: #abs means that if its like -7 then it becomes 7
+    left_x = 0
+if abs(left_y) < deadzone:
+    left_y = 0
+
