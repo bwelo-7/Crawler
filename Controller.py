03999f1846a54453ba888af9a25e4023beba7f19
ryan <github@ryanrae.com>
2025-06-18 18:06:35 +0100

    assigned both the axis of the left joystick to a variable

--- Controller.py
+++ Controller.py
@@ -5,3 +5,8 @@ pygame.joystick.init()
 if pygame.joystick.get_count() == 0:
     print('Mate plug the controller in')
 
+joystick = pygame.joystick.Joystick()
+joystick.init()
+
+left_x = joystick.get_axis(0)
+left_y = joystick.get_axis(1)
