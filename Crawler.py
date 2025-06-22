40fbf989db6cfa3a0df6e5cdb0b4c8b895c5cdeb
ryan <github@ryanrae.com>
2025-06-22 21:28:07 +0100

    bob shoots fireballs(forgot to commit lol) adding a delay now coz atm they just fire nonstop when R2 is held down

--- Crawler.py
+++ Crawler.py
@@ -29,10 +29,20 @@ if pygame.joystick.get_count() == 0:
 joystick = pygame.joystick.Joystick(0)
 joystick.init()
 
+
+fireballs = pygame.sprite.Group()
+
 all_sprites = pygame.sprite.Group()
+
 all_sprites.add(Bob(200, 200))
+
 all_sprites.add(Bill(300, 300))
-all_sprites.add(Fireball(200 ,300))
+
+Fireball_cooldown = 500
+last_fireball_time = 0
+
+
+
 #game loop
 running = True
 while running:
@@ -43,10 +53,25 @@ while running:
             #check for closing the window
             if event.type == pygame.QUIT:
                 running = False
+
+
     keys = pygame.key.get_pressed()
+    current_time = pygame.time.get_ticks()
+    R2_axis = joystick.get_axis(5)
+
     # Update
+    if R2_axis > 0.5:
+        for sprite in all_sprites:
+            if isinstance(sprite, Bob):
+                if current_time - last_fireball_time > Fireball_cooldown:
+                    fireball = Fireball(sprite.rect.centerx, sprite.rect.centery, dx = 0, dy= 10 )
+                    all_sprites.add(fireball)
+                    last_fireball_time = current_time
+
+
     for sprite in all_sprites:
         sprite.update(keys, joystick)
+
     # Draw / render
     screen.fill(BLACK)
     all_sprites.draw(screen)
--- Fireball.py
+++ Fireball.py
@@ -1,20 +1,20 @@
 import pygame
 from Bob import Thing
-from stuff import ORANGE, proj_width, proj_height, ax, ay, speed
+from stuff import ORANGE, proj_width, proj_height, ax, ay, speed, WIDTH, HEIGHT
 
 class Fireball(Thing):
-    def __init__(self, x, y):
-        Thing.__init__(self,x,y)
+    def __init__(self, x, y, dx = 0, dy = 10):
+        super().__init__(x, y)
         self.image = pygame.Surface((proj_width, proj_height))
         self.image.fill(ORANGE)
+        self.rect = self.image.get_rect(center=(x, y))
+        self.dx = 10
+        self.dy = 0
 
-    def update(self, keys=None, joystick=None):
-        pass
+    def update(self,keys, joystick):
+        self.rect.x += int(self.dx)
+        self.rect.y += int(self.dy)
 
-    def shoot(self, keys):
-        if keys[pygame.K_SPACE]:
-            draw = True
-            self.dy + ay
-            print(self.rect.y)
 
-        self.rect.y += self.dy
+        if self.rect.left > WIDTH or self.rect.right < 0 or self.rect.top > HEIGHT or self.rect.bottom < 0:
+            self.kill()
